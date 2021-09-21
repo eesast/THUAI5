94e6e517f8d3b75f0f4c4b7acd88ea959add48b7
TCL <1620508360@qq.com>
2021-09-21 13:46:37 +0800

    chore: Add abstract class GameObj and MoveableObj

---
+++ logic/Preparation/GameObj/MoveableObj.cs
@@ -0,0 +1,52 @@
+﻿using Preparation.Interface;
+using Preparation.Utility;
+
+namespace Preparation.GameObj
+{
+    public abstract class MoveableObj: GameObj,IMoveable
+    {
+        private int moveSpeed;
+        public int MoveSpeed
+        {
+            get => moveSpeed;
+            set 
+            {
+                lock(gameObjLock)
+                {
+                    moveSpeed = value;
+                }
+            }
+        }
+        protected readonly object moveLock = new object();
+        public override ShapeType Shape => ShapeType.Circle; //会移动的一定是圆形
+        public MoveableObj(XYPosition initPos, int initRadius, PlaceType initPlace,int initSpeed):base(initPos,initRadius,initPlace)
+        {
+            moveSpeed = initSpeed;
+        }
+        public long Move(Vector moveVec)
+        {
+            var XYVec = Vector.Vector2XY(moveVec);
+            lock (moveLock)
+            {
+                FacingDirection = moveVec.angle;
+                this.Position += XYVec;
+            }
+            return (long)(XYVec.ToVector2()* new Vector2(0, 0));
+        }
+
+        public virtual void Reset() //复活时数据重置
+        {
+            lock (moveLock)
+            {
+                this.Position = birthPos;
+            }
+            lock(gameObjLock)
+            {
+                FacingDirection = 0.0;
+                IsMoving = false;
+                CanMove = false;
+                IsResetting = true;
+            }
+        }
+    }
+}
