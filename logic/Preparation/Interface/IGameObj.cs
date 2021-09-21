d5983f236108f4ddd3c2a85e8abdba8497b72eff
刘浩然 <1042742166@qq.com>
2021-09-21 16:43:01 +0800

    refactor!: change Preparation to Class Library, Move ALL enum types from their Interface/GameObj files to a new file EnumType.cs, Add Constant.cs.

--- logic/Preparation/Interface/IGameObj.cs
+++ logic/Preparation/Interface/IGameObj.cs
@@ -2,19 +2,6 @@
 
 namespace Preparation.Interface
 {
-    public enum PlaceType  //位置标志，包括陆地，草丛，以及角色技能带来的隐身。
-    {
-        Land = 0,
-        Grass1 = 1,
-        Grass2 = 2,
-        Grass3 = 3,
-        Invisible = 4
-    }
-    public enum ShapeType
-    {
-        Circle = 0,      //仍然，子弹和人物为圆形，格子为方形
-        Square = 1
-    }
     public interface IGameObj
     {
         public long ID { get; }
---
+++ logic/Preparation/Utility/EnumType.cs
@@ -0,0 +1,24 @@
+﻿using Preparation.GameObj;
+using Preparation.Interface;
+
+namespace Preparation.Utility
+{
+    public enum GameObjType
+    {
+        Player = 0,
+        Object = 1
+    }
+    public enum ShapeType
+    {
+        Circle = 0,      //仍然，子弹和人物为圆形，格子为方形
+        Square = 1
+    }
+    public enum PlaceType  //位置标志，包括陆地，草丛，以及角色技能带来的隐身。
+    {
+        Land = 0,
+        Grass1 = 1,
+        Grass2 = 2,
+        Grass3 = 3,
+        Invisible = 4
+    }
+}
