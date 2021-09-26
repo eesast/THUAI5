79fe47e26154129ae8bd5474813b37652ea6ecf2
Ther <1329438302@qq.com>
2021-09-26 20:06:43 +0800

    fix: change .bat files to .cmd, and optimize the command line parameter settings

--- communication/ClientTest/Program.cs
+++ communication/ClientTest/Program.cs
@@ -6,20 +6,37 @@ namespace ClientTest
 {
     class ClientTest
     {
+        /// <summary>
+        /// args[0] 队伍编号(必填) args[1] 玩家编号(必填) args[2] ip(选填,默认127.0.0.1) args[3] port(选填,默认7777)
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string ip = "127.0.0.1";
+            ushort port = 7777;
             try
             {
                 Console.WriteLine($"my params are {int.Parse(args[0])} and {int.Parse(args[1])}");
-                Console.WriteLine($"connect to ip {args[2]}");
-                ip = args[2];
+                Console.WriteLine($"connect to ip {args[2]} with port {args[3]}");
+                
+                // 当然我知道这样不太好，应该把ip和port改成默认参数，即使在命令行参数里面不输入也能以默认方式连接
+                // TODO 以后把这里改一下
             }
             catch(Exception e)
             {
-                Console.WriteLine("wrong params!");
+                Console.WriteLine("team id and player id are required!");
                 Environment.Exit(0); // 我也不知道这样做合适不合适...
             }
+
+            if (args.Length >= 3)
+            {
+                ip = args[2];
+                if(args.Length >= 4)
+                {
+                    port = ushort.Parse(args[3]);
+                }
+            }
+           
             ClientCommunication client = new ClientCommunication();
             client.OnReceive += delegate ()
             { 
@@ -29,7 +46,7 @@ namespace ClientTest
                 Console.WriteLine(m2one);
             };
 
-            if (client.Connect(ip,7777))
+            if (client.Connect(ip,port))
             {
                 Console.WriteLine("success to connect to the server.");
             }
