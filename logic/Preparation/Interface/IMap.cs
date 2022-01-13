32ce41b6eafd4e603659cf4ed71796d70a6e5fb6
TCL <1620508360@qq.com>
2022-01-14 00:02:54 +0800

    fix: fix bugs of walking in wall

--- logic/Preparation/Interface/IMap.cs
+++ logic/Preparation/Interface/IMap.cs
@@ -9,8 +9,10 @@ namespace Preparation.Interface
         ITimer Timer { get; }
         List<ICharacter> PlayerList { get; }
         List<IObjOfCharacter> BulletList { get; }
+        List<IGameObj> MapObjList { get; }
         ReaderWriterLockSlim PlayerListLock { get; }
         ReaderWriterLockSlim BulletListLock { get; }
+        ReaderWriterLockSlim MapObjListLock { get; }
         public bool IsWall(XYPosition pos);
         public bool IsOutOfBound(IGameObj obj);
         public IOutOfBound GetOutOfBound(XYPosition pos); //返回新建的一个OutOfBound对象
