426a87a5e54aa185cb623297f52e42c1436b812b
TCL <1620508360@qq.com>
2022-03-07 23:23:38 +0800

    refactor: use frameRateTask from nuget

--- logic/FrameRateTask/README.md
+++
@@ -1,3 +0,0 @@
-# FrameRateTask
-
-The introduction of this project is in the README.md of [the github repository FrameRateTask of Timothy-LiuXuefeng](https://github.com/Timothy-LiuXuefeng/FrameRateTask) (Click to enter).
--- logic/cmd/testClient.cmd
+++ logic/cmd/testClient.cmd
@@ -2,4 +2,6 @@
 
 start cmd /k ..\Test\bin\Debug\net5.0\Test.exe 0 1
 
-start cmd /k ..\Client\bin\Debug\net5.0-windows\Client.exe
+start cmd /k ..\Test\bin\Debug\net5.0\Test.exe 0 0
+
+#start cmd /k ..\Client\bin\Debug\net5.0-windows\Client.exe
