df59d10edfe64c243c55761e17ed3cf8399e6efc
TCL <1620508360@qq.com>
2022-03-15 22:34:14 +0800

    test: add test cmd

--- logic/cmd/gameServer.cmd
+++ logic/cmd/gameServer.cmd
@@ -2,4 +2,4 @@
 
 start cmd /k ..\Server\bin\Debug\net5.0\Server.exe --port=7777 --teamCount=2 --playerCount=1  --gameTimeInSecond=300 --mapResource=testmap1.txt
 
-#start cmd /k ..\Client\bin\Debug\net5.0-windows\Client.exe -u --port=7777 --teamID=3330 --characterID=3330 --software=2 --hardware=2
+start cmd /k ..\Client\bin\Debug\net5.0-windows\Client.exe --cl --port=7777 --teamID=3330 --characterID=3330 --software=2 --hardware=2
