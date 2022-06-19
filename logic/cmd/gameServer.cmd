0f7defb3a4f90825d59272d15659fff61453e718
刘浩然 <1042742166@qq.com>
2022-06-20 00:22:39 +0800

    chore: resolve confilcts.

--- logic/cmd/gameServer.cmd
+++ logic/cmd/gameServer.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-start cmd /k ..\Server\bin\Debug\net5.0\Server.exe --port 7777 --teamCount 2 --playerCount 1 --gameTimeInSecond 600 --fileName video  --mapResource=mapforfun.txt
+start cmd /k ..\Server\bin\Debug\net5.0\Server.exe --port 7777 --teamCount 1 --playerCount 1 --gameTimeInSecond 600 --fileName test --mapResource=./maps/testmap.txt
 
-start cmd /k ..\Client\bin\Debug\net5.0-windows\Client.exe --cl --port=7777 --teamID=1 --characterID=0 --software=3 --hardware=3
+start cmd /k ..\Client\bin\Debug\net5.0-windows\Client.exe --cl --port=7777 --teamID=0 --characterID=0 --software=3 --hardware=3
 
-start cmd /k ..\CSharpInterface\bin\Debug\net5.0\CSharpInterface.exe 0 0
+::start cmd /k ..\CSharpInterface\bin\Debug\net5.0\CSharpInterface.exe 0 0
